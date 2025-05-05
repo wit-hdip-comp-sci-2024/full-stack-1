00cb29defb06d739967e31fb2b6a1fa808ecf3f8
Eamonn de Leastar <edeleastar@wit.ie>
2025-05-05 07:26:37 +0100

    topic 23 donation app

---
+++ prj/donation/svelte/donation-svelte-08-ssr/src/lib/services/donation-service.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+import type { Session, User } from "$lib/types/donation-types";
+import type { Candidate, Donation } from "$lib/types/donation-types";
+
+export const donationService = {
+  baseUrl: "http://localhost:4000",
+  //  baseUrl: "https://donation-web-api-ts.glitch.me",
+
+  async signup(user: User): Promise<boolean> {
+    try {
+      const response = await axios.post(`${this.baseUrl}/api/users`, user);
+      return response.data.success === true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+
+  async login(email: string, password: string): Promise<Session | null> {
+    try {
+      const response = await axios.post(`${this.baseUrl}/api/users/authenticate`, {
+        email,
+        password
+      });
+      if (response.data.success) {
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.token;
+        const session: Session = {
+          name: response.data.name,
+          token: response.data.token,
+          _id: response.data._id
+        };
+        return session;
+      }
+      return null;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  },
+
+  async donate(donation: Donation, token: string) {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      const response = await axios.post(this.baseUrl + "/api/candidates/" + donation.candidate + "/donations", donation);
+      // response.status == 200;
+      return response.data
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+
+  async getCandidates(token: string): Promise<Candidate[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      const response = await axios.get(this.baseUrl + "/api/candidates");
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  },
+
+  async getDonations(token: string): Promise<Donation[]> {
+    try {
+      axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+      const response = await axios.get(this.baseUrl + "/api/donations");
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+};
---
+++ prj/donation/svelte/donation-svelte-08-ssr/src/routes/report/+page.server.ts
@@ -0,0 +1,12 @@
+import { donationService } from "$lib/services/donation-service";
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async ({ parent }) => {
+  const { session } = await parent();
+  if (session) {
+    return {
+      donations: await donationService.getDonations(session.token),
+      candidates: await donationService.getCandidates(session.token)
+    };
+  }
+};
